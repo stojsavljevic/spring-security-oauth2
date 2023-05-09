860485995a01a5627b51aa5dd543e260f2633a1f
Aleksandar Stojsavljevic <aleksandars@ccbill.com>
2023-05-09 09:39:19 +0200

    Renamed main classes

--- spring-security-oauth2-client/src/main/java/alex/spring/security/client/OAuth2ClientApplication.java
+++ spring-security-oauth2-client/src/main/java/alex/spring/security/client/OAuth2ClientApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeycloakClientApplication {
+public class OAuth2ClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KeycloakClientApplication.class, args);
+		SpringApplication.run(OAuth2ClientApplication.class, args);
 	}
 }
--- spring-security-oauth2-resource-server/src/main/java/alex/spring/security/resource/OAuth2ResourceServerApplication.java
+++ spring-security-oauth2-resource-server/src/main/java/alex/spring/security/resource/OAuth2ResourceServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeycloakResourceServerApplication {
+public class OAuth2ResourceServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KeycloakResourceServerApplication.class, args);
+		SpringApplication.run(OAuth2ResourceServerApplication.class, args);
 	}
 }
