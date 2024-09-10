271cdbd68c71dbbd4cd741931afe52eb361feb7d
Aleksandar Stojsavljevic <sasa.stojsavljevic@gmail.com>
2024-09-10 17:33:50 +0200

    Updated to SB 3.3.3; Added tests

---
+++ spring-security-oauth2-client/src/test/java/com/alex/oauth2/client/OAuth2ClientTests.java
@@ -0,0 +1,14 @@
+package com.alex.oauth2.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class OAuth2ClientTests {
+
+    @Test
+    void contextLoads() {
+        // This test will automatically fail if the application context cannot be loaded.
+    }
+}
---
+++ spring-security-oauth2-resource-server/src/test/java/com/alex/oauth2/resource/OAuth2ResourceServerTests.java
@@ -0,0 +1,14 @@
+package com.alex.oauth2.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class OAuth2ResourceServerTests {
+
+    @Test
+    void contextLoads() {
+        // This test will automatically fail if the application context cannot be loaded.
+    }
+}
